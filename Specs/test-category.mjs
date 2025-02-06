cc737e2925d669be7f38472aff841c64829ed392
Eric Jacobson <contact@efjacobson.com>
2025-02-05 19:57:39 -0800

    new world order

---
+++ Specs/test-category.mjs
@@ -0,0 +1,16 @@
+import { assert } from 'chai';
+
+describe('test-category', function() {
+  it('validateCategoryById', function() {
+    assert.strictEqual(1, 1);
+  });
+  it('validateCategoryById', function() {
+    assert.strictEqual(1, 2);
+  });
+  it('someOtherTestName', function() {
+    assert.strictEqual(1, 1);
+  });
+  it('whateverYouWant', function() {
+    assert.strictEqual(1, 2);
+  });
+});
--- test.mjs
+++ test.mjs
@@ -1,16 +1,111 @@
+#! /usr/bin/env node
 
-const passingHardcodedAutomationId = './tests.mjs:testSpecificThingThatPasses';
-const failingHardcodedAutomationId = './tests.mjs:testSpecificThingThatFails';
+import { access, readFile } from 'node:fs/promises';
+import { parseArgs } from 'node:util';
+import Mocha from 'mocha';
+import os from 'node:os';
 
-describe('demo', () => {
-  it('should run a test that passes', async () => {
-    const [filePath, fn] = passingHardcodedAutomationId.split(':');
-    const file = await import(filePath);
-    file[fn]();
-  });
-  it('should run a test that fails', async () => {
-    const [filePath, fn] = failingHardcodedAutomationId.split(':');
-    const file = await import(filePath);
-    file[fn]();
-  });
+const options = {
+  username: {
+    type: 'string',
+  },
+  origin: {
+    type: 'string',
+  },
+  'project-id': {
+    type: 'string',
+    default: '218',
+  },
+  'suite-id': {
+    type: 'string',
+    default: '76158',
+  },
+};
+
+const { values } = parseArgs({
+  options,
+  strict: true,
 });
+
+const { username, origin, 'project-id': projectId, 'suite-id': suiteId } = values;
+
+if (!username) {
+  console.log('You must provide your TestRail username.');
+  process.exit(1);
+}
+
+if (!origin) {
+  console.log('You must provide the TestRail origin.');
+  process.exit(1);
+}
+
+const apiKeyPath = `${os.homedir()}/.testrail_api_key`;
+try {
+  await access(apiKeyPath);
+} catch (e) {
+  console.log(`You need to create a file at ${apiKeyPath} with your TestRail API key.`);
+  process.exit(1);
+}
+
+let apiKey;
+try {
+  apiKey = await readFile(apiKeyPath, 'utf8');
+  apiKey = apiKey.trim();
+} catch (e) {
+  console.log('Something is wrong with your Testrail API key.')
+  process.exit(1);
+}
+
+const getAutomationIds = async () => {
+  const response = await fetch(
+    `${origin}/index.php?/api/v2/get_cases/${projectId}&suite_id=${suiteId}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${btoa(`${username}:${apiKey}`)}`
+      }
+    },
+  );
+  const data = await response.json();
+  return data.cases.reduce((acc, testCase) => {
+    if (!testCase.custom_automation_id) {
+      return acc;
+    }
+    const split = testCase.custom_automation_id.split(':');
+    if (split.length !== 2) {
+      return acc;
+    }
+    acc.push(testCase.custom_automation_id);
+    return acc;
+  }, [])
+};
+
+(async () => {
+  const automationIds = await getAutomationIds();
+
+  const filePaths = automationIds.map(automationId => automationId.split(':')[0]);
+  const set = new Set();
+  while (filePaths.length > 0) {
+    let filePath = filePaths.pop();
+    const extension = filePath.split('.').pop();
+    if (extension !== 'mjs') {
+      filePath = `${filePath.split('.').slice(0, -1).join('.')}.mjs`;
+    }
+    try {
+      await access(filePath);
+      set.add(filePath);
+    } catch (e) {
+      console.log(`Could not find file ${filePath}`);
+    }
+  }
+
+  const mocha = new Mocha();
+  set.forEach(file => mocha.addFile(file));
+
+  const testNames = automationIds.map(automationId => automationId.split(':')[1]);
+  await mocha.grep(new RegExp(`(${testNames.join('|')})`));
+
+  await mocha.loadFilesAsync();
+  await mocha.run();
+})();
