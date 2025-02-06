6cd58e248d5790d98b25353f04234403beb33737
Eric Jacobson <contact@efjacobson.com>
2025-02-05 20:02:29 -0800

    show that it doesnt execute every file

---
+++ Specs/test-something.mjs
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+
+describe('test-something', function() {
+  it('willExecuteThisTestCaseIfYouUpdateATestRailCaseToHaveTheRightAutomationId', function() {
+    assert.strictEqual(1, 1);
+  });
+});
